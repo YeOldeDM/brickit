fa68510109adde9b3c3c51536c497b436d3ef57f
TheSkyOne <omer9955821@gmail.com>
2018-10-21 21:44:53 +0300

    highscore is now saved and loaded upon relaunch

--- PROJECT/Global/GLOBAL.gd
+++ PROJECT/Global/GLOBAL.gd
@@ -1,16 +1,30 @@
 extends Node
 
 const BRICK_VALUE = 50
-
-
-
+const FILE_PATH = "user://highscore.data"
 
 signal score_changed(to)
 signal high_score_changed(to)
 
 var score = 0 setget _set_score
+var high_score = 0 setget _set_high_score
 
-var high_score = 5000 setget _set_high_score
+func _ready():
+	load_highscore()
+
+func load_highscore():
+	var file = File.new()
+	if !file.file_exists(FILE_PATH):
+		return
+	file.open(FILE_PATH, file.READ)
+	high_score = file.get_var()
+	file.close()
+	
+func save_highscore():
+	var file = File.new()
+	file.open(FILE_PATH, file.WRITE)
+	file.store_var(high_score)
+	file.close()
 
 func _set_score( to ):
 	score = to
@@ -23,3 +37,4 @@ func _set_score( to ):
 func _set_high_score(to):
 	high_score = to
 	emit_signal("high_score_changed", to)
+	save_highscore()
--- PROJECT/Title/Title.gd
+++ PROJECT/Title/Title.gd
@@ -1,7 +1,4 @@
 extends Control
 
-
-
-
 func _on_TextureButton_pressed():
 	get_tree().change_scene( "res://Game/Game.tscn" )
