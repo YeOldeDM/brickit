429cbb5feeaca2b04faf6dbfe7c4e1077ca19d91
YeOldeDM <nines@email.com>
2018-10-29 13:38:49 -0600

    different kind of paddle deflection

--- PROJECT/Ball/Ball.gd
+++ PROJECT/Ball/Ball.gd
@@ -7,6 +7,17 @@ const YLIMIT = 260
 # How much ball is deflected left/right when hitting the edge of the paddle
 const PADDLE_DEFLECT = 120
 
+var deflects = [
+	-150,
+	-75,
+	-32,
+	0,
+	32,
+	75,
+	150,
+]
+
+
 var velocity = Vector2(180,600) #must begin with some Y velocity!
 
 signal get_paddle_pos()
@@ -20,8 +31,15 @@ func _physics_process(delta):
 		var col = motion.collider
 		if col: #bounce off of solids
 			velocity = velocity.bounce( motion.normal )
-#			if col.is_in_group("paddle"): # Deflect when hitting the paddle.
-#				var x_diff =  position.x - col.position.x
+			if col.is_in_group("paddle"): # Deflect when hitting the paddle.
+				var x_diff =  position.x - col.position.x # Should be -32|32
+				var a = 64/6	#length of a section
+				var deflection_section = round(x_diff/a) #will be -3|3
+				var def_x = deflects[3+deflection_section] #Convert to 0|6
+#				print(def_x)
+				velocity.x = def_x
+				
+				
 #				velocity.x = x_diff * PADDLE_DEFLECT
 			# if we hit a brick
 			if col.is_in_group("brick"):
@@ -40,34 +58,34 @@ func _physics_process(delta):
 		velocity = -Vector2(0,600)
 
 	# maintain a constant vertical speed
-#	velocity.y = YLIMIT * sign(velocity.y)
-#	if abs( velocity.x ) > XLIMIT:
-#		velocity.x = XLIMIT * sign(velocity.x)
+	velocity.y = YLIMIT * sign(velocity.y)
+	if abs( velocity.x ) > XLIMIT:
+		velocity.x = XLIMIT * sign(velocity.x)
 
 func _on_Paddle_send_paddle_pos(paddle_pos):
 	position.x = paddle_pos.x
 	position.y = paddle_pos.y - 8
 
-func _on_Paddle_ll():
-	velocity = Vector2(-150,-150)
-	print("ll")
-
-func _on_Paddle_lr():
-	velocity = Vector2(-75,-75)
-	print("lr")
-
-func _on_Paddle_ml():
-	velocity = Vector2(-32,-32)
-	print("ml")
-
-func _on_Paddle_mr():
-	velocity = Vector2(32,-32)
-	print("mr")
-
-func _on_Paddle_rl():
-	velocity = Vector2(75,-75)
-	print("rl")
-
-func _on_Paddle_rr():
-	velocity = Vector2(150,-150)
-	print("rr")
+#func _on_Paddle_ll():
+#	velocity = Vector2(-150,-150)
+#	print("ll")
+#
+#func _on_Paddle_lr():
+#	velocity = Vector2(-75,-75)
+#	print("lr")
+#
+#func _on_Paddle_ml():
+#	velocity = Vector2(-32,-32)
+#	print("ml")
+#
+#func _on_Paddle_mr():
+#	velocity = Vector2(32,-32)
+#	print("mr")
+#
+#func _on_Paddle_rl():
+#	velocity = Vector2(75,-75)
+#	print("rl")
+#
+#func _on_Paddle_rr():
+#	velocity = Vector2(150,-150)
+#	print("rr")
--- PROJECT/Paddle/Paddle.gd
+++ PROJECT/Paddle/Paddle.gd
@@ -27,31 +27,31 @@ func _physics_process(delta):
 		input = joy_x
 	
 	var movement =  move_and_collide( Vector2( input*SPEED,0 )*delta )
-	if	movement: # here im supposed to get through if there is a collider
-		var col = movement.collider
-		if col and col.is_in_group("balls"): #here if the collision is with the ball
-			print("works")
-			#from here on out  i check which section of the paddle the ball hit
-			#there are six sections, the letter 'l' represents left, 'r' right and 'm' middle.
-			#then it emits signals to ball, and there depending on which section was hit, 
-			#the ball gets different velocity vector to go in a different direction
-			if movement.position.x > position.x - 24 and movement.position.x <= position.x - 16:
-				emit_signal("ll")
-				
-			elif movement.position.x > position.x - 16 and movement.position.x <= position.x - 8:
-				emit_signal("lr")
-				
-			elif movement.position.x > position.x - 8 and movement.position.x <= position.x:
-				emit_signal("ml")
-				
-			elif movement.position.x > position.x and movement.position.x <= position.x + 8:
-				emit_signal("mr")
-				
-			elif movement.position.x > position.x + 8 and movement.position.x <= position.x + 16:
-				emit_signal("rl")
-				
-			elif movement.position.x > position.x + 16 and movement.position.x <= position.x + 24:
-				emit_signal("rr")
+#	if	movement: # here im supposed to get through if there is a collider
+#		var col = movement.collider
+#		if col and col.is_in_group("balls"): #here if the collision is with the ball
+#			print("works")
+#			#from here on out  i check which section of the paddle the ball hit
+#			#there are six sections, the letter 'l' represents left, 'r' right and 'm' middle.
+#			#then it emits signals to ball, and there depending on which section was hit, 
+#			#the ball gets different velocity vector to go in a different direction
+#			if movement.position.x > position.x - 24 and movement.position.x <= position.x - 16:
+#				emit_signal("ll")
+#
+#			elif movement.position.x > position.x - 16 and movement.position.x <= position.x - 8:
+#				emit_signal("lr")
+#
+#			elif movement.position.x > position.x - 8 and movement.position.x <= position.x:
+#				emit_signal("ml")
+#
+#			elif movement.position.x > position.x and movement.position.x <= position.x + 8:
+#				emit_signal("mr")
+#
+#			elif movement.position.x > position.x + 8 and movement.position.x <= position.x + 16:
+#				emit_signal("rl")
+#
+#			elif movement.position.x > position.x + 16 and movement.position.x <= position.x + 24:
+#				emit_signal("rr")
 		
 
 	if position.y != _y:
